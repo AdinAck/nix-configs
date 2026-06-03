3a0bbd490799553c7f80d3adf7f993e4c07d6110
Adin Ackerman <adinackerman@gmail.com>
2026-06-02 20:36:21 -0700

    gnome settings

--- user/gnome.nix
+++ user/gnome.nix
@@ -1,11 +1,49 @@
+{ lib, ... }:
+with lib.hm.gvariant;
 {
-	programs.gnome-terminal = {
-		enable = true;
-		profile.default = {
-			default = true;
-			visibleName = "Default";
-			useSystemFont = false;
-			font = "MesloLGDZ Nerd Font Mono 11";
-		};
-	};
+  dconf.settings = {
+    "org/gnome/Console" = {
+      custom-font = "MesloLGLDZ Nerd Font Mono 10";
+      theme = "night";
+      use-system-font = false;
+    };
+
+    "org/gnome/desktop/background" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/tubes-l.jxl";
+      picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/tubes-d.jxl";
+      primary-color = "#000000";
+      secondary-color = "#000000";
+    };
+
+    "org/gnome/desktop/interface" = {
+      accent-color = "orange";
+      color-scheme = "prefer-dark";
+    };
+
+    "org/gnome/desktop/screensaver" = {
+      color-shading-type = "solid";
+      picture-options = "zoom";
+      picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/tubes-l.jxl";
+      primary-color = "#000000";
+      secondary-color = "#000000";
+    };
+
+    "org/gnome/desktop/session" = {
+      idle-delay = mkUint32 0;
+    };
+
+    "org/gnome/mutter" = {
+      workspaces-only-on-primary = false;
+    };
+
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-schedule-automatic = false;
+    };
+
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
+    };
+  };
 }
